b3bf7d65a0e3f30816ae7c4d77bcb7cdc6b691f9
Rishi-077 <rishikesan070@gmail.com>
2022-01-30 21:06:46 +0530

    firdt commit

---
+++ src/Components/List.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function List ({ people }) {
+    return ( 
+        <>
+            {
+            people.map((person) => {
+                const { id, name, age, image } = person;
+                return (
+                    <article key={id} className='person'>
+                        <img className='img-fluid float-start' src={image} alt={name} />
+                        <div>
+                            <h3>{name}</h3>
+                            <p>{age} years</p>
+                        </div>
+                    </article>
+                );
+            })
+            } 
+        </>
+      
+     );
+}
+
+export default List;
